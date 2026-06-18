f2233075e41a3e7dc07a66480c516750dd53fd7f
unknown <hybiekay2@gmail.com>
2026-06-18 16:12:08 +0100

    Introduce PageRegistry and deprecate FlintComponentRegistry

--- lib/ui/main.dart
+++ lib/ui/main.dart
@@ -1,6 +1,6 @@
 import 'package:flint_dart/flint_ui.dart';
 
-import 'component_registry.dart';
+import 'page_registry.dart';
 import 'styles/app_root_design.dart';
 
 void main() {
--- lib/ui/page_registry.dart
+++ lib/ui/page_registry.dart
@@ -5,7 +5,7 @@ import 'pages/login_page.dart';
 import 'pages/register_page.dart';
 import 'pages/welcome_page.dart';
 
-final componentRegistry = FlintComponentRegistry({
+final componentRegistry = PageRegistry({
   'Welcome': (props) => WelcomePage(props),
   'Dashboard': (props) => DashboardPage(props),
   'Login': (props) => LoginPage(props),
